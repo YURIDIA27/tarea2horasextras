f5b7075f9053238df28284b5b53f3535ebd52a1d
YURIDIA27 <cuellar.27y@gmail.com>
2024-10-07 01:23:25 -0600

    Create tarea2.py

---
+++ tarea2.py
@@ -0,0 +1,20 @@
+# realizar el programa de horas extras
+
+Hrs= int (entrada( ingrese una cantidad))
+
+si Hrs>8:
+
+jorpago= 8*200
+
+hrsextras= Hrs-8
+Pagoex= Hrsextras*350
+pagofinal=jorpago + pagoex
+# "impresion" (pagofinal)
+
+Pago normal:
+
+jorpago= Hrs * 200
+    pagofinal=jorpago
+    print (pagofinal)
+
+"impresion" (fin del programa)
