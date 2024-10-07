fcf4edfac7f99f5226c06fd0a69a144373ee0068
YURIDIA27 <cuellar.27y@gmail.com>
2024-10-07 01:26:13 -0600

    Update calficaciones.py

--- calficaciones.py
+++ calficaciones.py
@@ -1,8 +1,8 @@
 #realize un programa que lea 3 calificaciones y su promedio
 
-calf1=int(input ("ingrese calficación: "))
-calf2=int(input ("ingrese calificación "))
-calf3=int(input ("ingrese calificación "))
+calf1=int(imput ("ingrese calficación: "))
+calf2=int(imput ("ingrese calificación "))
+calf3=int(imput ("ingrese calificación "))
 
 promedio= (calf1 + calf2 + calf3) / 3
 
