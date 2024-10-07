b8ad9aed812bfb8a0eaadf0ba949645fe66ca69a
YURIDIA27 <cuellar.27y@gmail.com>
2024-10-07 01:18:31 -0600

    Create calficaciones.py

---
+++ calficaciones.py
@@ -0,0 +1,11 @@
+#realize un programa que lea 3 calificaciones y su promedio
+
+calf1=int(input ("ingrese calficación: "))
+calf2=int(input ("ingrese calificación "))
+calf3=int(input ("ingrese calificación "))
+
+promedio= (calf1 + calf2 + calf3) / 3
+
+print (promedio)
+
+print ("fin del programa")
